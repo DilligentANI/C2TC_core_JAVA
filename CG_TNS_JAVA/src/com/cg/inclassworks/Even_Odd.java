3be342523c26abfe2f6ebe005d43ee6be115d6a7
anike <anike@LAPTOP-N73G34HJ>
2022-08-20 11:36:13 +0530

    In class Program

--- CG_TNS_JAVA/src/com/cg/inclassworks/Even_Odd.java
+++ CG_TNS_JAVA/src/com/cg/inclassworks/Even_Odd.java
@@ -1,5 +1,20 @@
 package com.cg.inclassworks;
 
-public class Even_Odd {
+import java.util.Scanner;
 
-}
+public class Even_Odd {
+	
+	public static void main(String[] args) {
+	
+		Scanner sc=new Scanner(System.in); //Object creation 
+		System.out.println("Enter a number:");	
+		int a=sc.nextInt();
+		if(a%2==0)
+				{
+					System.out.println("is even");
+				}
+		else {
+				System.out.println("odd");
+			 }
+		}
+	}
